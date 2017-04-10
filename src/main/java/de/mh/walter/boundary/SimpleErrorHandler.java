c8b672691952f29bfba806510048e1c8a611973b
Marco Haase <github@marco-haase.de>
2017-04-10 16:43:31 +0200

    enhanced error controller

--- src/main/java/de/mh/walter/boundary/SimpleErrorHandler.java
+++ src/main/java/de/mh/walter/boundary/SimpleErrorHandler.java
@@ -1,15 +1,23 @@
 package de.mh.walter.boundary;
 
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @RestController
 public class SimpleErrorHandler implements ErrorController {
 
+    @Autowired
+    private ErrorAttributes errorAttributes;
+    //
     private static final String PATH = "/error";
 
     @Override
@@ -20,7 +28,13 @@ public class SimpleErrorHandler implements ErrorController {
     @RequestMapping(value = PATH)
     public String error(HttpServletRequest request, HttpServletResponse response) {
         String errorText = response.getStatus() + " / " + HttpStatus.valueOf(response.getStatus()).getReasonPhrase();
-        System.out.println("error occurred: " + errorText);
+        Map<String, Object> errorMap = getErrorAttributes(request, false);
+        System.out.println("error occurred: " + errorText + "; error: " + errorMap.get("error") + "; message: " + errorMap.get("message") + "");
         return errorText;
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
     }
 }
